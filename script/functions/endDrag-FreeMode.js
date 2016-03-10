62a071836ca3f27e37ca27cabbca1a6585a79033
caegrivol <cgrivol@gmail.com>
2016-03-10 22:14:27 +0100

    Permitted Game Zone

--- script/functions/endDrag-FreeMode.js
+++ script/functions/endDrag-FreeMode.js
@@ -4,8 +4,14 @@ function endDrag(tempSprite, pointer) {
 	if ((game.input.y > 20) && (game.input.y < 96) && (game.input.x > 60) && (game.input.x < 143)) {
 		deleteSprite(tempSprite);
 	} else {
-		snapEffect(tempSprite,'freeMode');
-		colorSprite(tempSprite,'freeMode');
+	    if ((game.input.y > (0.7*game.height)) ||  (game.input.y < (0.12*game.height)) || (game.input.x > (0.8*game.width)) || (game.input.x < (0.2*game.width))) {
+	        tempSprite.x = Math.random()*(game.width-2*90)+90;
+	        tempSprite.y = game.height - 155;
+	    }
+	    else {
+		    snapEffect(tempSprite,'levelMode');
+			colorSprite(tempSprite,'levelMode');
+		}
 	}
-
+	updateSolution();
 }
--- script/functions/endDrag-Level.js
+++ script/functions/endDrag-Level.js
@@ -4,11 +4,16 @@ function endDrag(tempSprite, pointer) {
 	if ((game.input.y > 20) && (game.input.y < 96) && (game.input.x > 60) && (game.input.x < 143)) {
 		deleteSprite(tempSprite);
 	} else {
-		if (isDrag()) {
-			snapEffect(tempSprite,'levelMode');
-			colorSprite(tempSprite,'levelMode');
+	    if ((game.input.y > (0.7*game.height)) ||  (game.input.y < (0.12*game.height)) || (game.input.x > (0.8*game.width)) || (game.input.x < (0.2*game.width))) {
+	        tempSprite.x = Math.random()*(game.width-2*90)+90;
+	        tempSprite.y = game.height - 155;
+	    }
+	    else {
+		    if (isDrag()) {
+			    snapEffect(tempSprite,'levelMode');
+			    colorSprite(tempSprite,'levelMode');
+		    }
 		}
 	}
 	updateSolution();
-
 }
