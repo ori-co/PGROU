f537d64151f2547618feccc8dfccc96fb512ddee
caegrivol <cgrivol@gmail.com>
2016-03-12 12:00:32 +0100

    Game Zone (in pixels)

--- script/functions/endDrag-Level.js
+++ script/functions/endDrag-Level.js
@@ -4,7 +4,7 @@ function endDrag(tempSprite, pointer) {
 	if ((game.input.y > bin.y) && (game.input.y < bin.y+bin.height) && (game.input.x > bin.x) && (game.input.x < bin.x+bin.width)) {
 		deleteSprite(tempSprite);
 	} else {
-	    if ((game.input.y > (0.7*game.height)) ||  (game.input.y < (0.12*game.height)) || (game.input.x > (0.8*game.width)) || (game.input.x < (0.2*game.width))) {
+	    if ((tempSprite.y > 500) ||  (tempSprite.y < 25) || (tempSprite.x > 1000) || (tempSprite.x < 225)) {
 	        tempSprite.x = Math.random()*(game.width-2*90)+90;
 	        tempSprite.y = game.height - 155;
 	    }
@@ -12,10 +12,10 @@ function endDrag(tempSprite, pointer) {
                 if (isDrag()) {
                     // utilisation de la variable globale "mode" possible
                     // mutualisation de code avec endDrag-LevelMode.js
-                        snapEffect(tempSprite,'levelMode');
-                        //colorSprite(tempSprite,'levelMode');
+                    snapEffect(tempSprite,'levelMode');
                 }
 		}
+		//colorSprite(tempSprite,'levelMode');
 		colorSprite(tempSprite,'levelMode');
 	}
 	updateSolution();
