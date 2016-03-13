2cb460ddcf922f1f184719ecaa289ac98a56e4df
Oriane <oriane.cosson@eleves.ec-nantes.fr>
2016-03-13 17:20:00 +0100

    ajout Patrick dans load.html

---
+++ script/functions/animations.js
@@ -0,0 +1,16 @@
+
+function patrickSpeak(sound){
+    sound.options.onplay = function(){
+        patMouth.animations.play('talk',8,true);
+    } 
+    
+    sound.options.onfinish = function(){
+        patMouth.animations.stop(null,true);
+    }
+}
+
+function patrickBlink(){
+    patEyes.animations.play('blink',10,false);
+    var delay = Math.random()*3 +2;
+    game.time.events.add(Phaser.Timer.SECOND * delay, patrickBlink, this);
+}
