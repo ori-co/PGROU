99620102406540a5a2d3bd5d0b04d8b172c5ebda
Oriane <oriane.cosson@gmail.com>
2016-10-10 18:45:17 +0200

    wallpaper plus grand

--- script/functions/wallpaper.js
+++ script/functions/wallpaper.js
@@ -11,14 +11,13 @@ function menuWallpaper() {
     game.scale.scaleMode = Phaser.ScaleManager.RESIZE;    
 
     //Wallpaper
-    var myBitmap = game.make.bitmapData(2000, 2000);
+    var myBitmap = game.make.bitmapData(3000, 2000);
     myBitmap.addToWorld();
     var grd = myBitmap.context.createLinearGradient(0, 0, 1000, 1000);
     grd.addColorStop(0, "#66d1f9");
     grd.addColorStop(1, "#bce3f0");
     myBitmap.context.fillStyle = grd;
-    myBitmap.context.fillRect(0, 0, 2000, 2000);
+    myBitmap.context.fillRect(0, 0, 3000, 2000);
 
-    //game.stage.backgroundColor = "#4488AA";
     palette = ["0x83EE00", "0xFBFE00", "0x058AB6", "0xE4005C", "0x8007BE", "0xFF8300"];
 }
