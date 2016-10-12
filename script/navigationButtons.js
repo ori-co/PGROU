db92a491b1657908bc318120813c92994b0e560e
Oriane <oriane.cosson@gmail.com>
2016-10-12 10:18:58 +0200

    build home and levels map

--- script/navigationButtons.js
+++ script/navigationButtons.js
@@ -1,19 +1,41 @@
 function homeButtons() {
-	var toFreePlayButton = game.add.button(0,0, 'homeButton', goToFreePlay, this,  2,1, 0, 1 );
-	var toFreePlayButton = game.add.button(0,100, 'homeButton', goToLevelsMap, this,  2,1, 0, 1 );
+	var goToStyle = {font: "23px Arial", fontWeight: "bold", fill: "#0D004C"};
+
+	var goToLevelsMapButton = game.add.button(600,500, 'button-goTo', goToLevelsMap, this,  2,1, 0, 1 );
+	goToLevelsMapButton.addChild(game.make.text(30,30,levelmode,goToStyle));
+	var goToFreeMode = game.add.button(600,600, 'button-goTo', goToFreePlay, this,  2,1, 0, 1 );
+	goToFreeMode.addChild(game.make.text(30,30,freemode,goToStyle));
 }
 
 function levelsButtons(){
+	var levelStyle = {font: "30px Arial", fontWeight: "bold", fill: "#0D004C"};
+	
 	var levelsNumber = levels.length;
 	
-	for (i=0; i<levelsNumber;i++){
-		var li = Math.floor(i/10);
-		var col = i%10;
-		var temp = game.add.button(col*80,li*80,'levelButton', goToLevelPlay, this, 2,1,0,1);
+	for (i=0; i<levelsNumber-1;i++){
+		var li = Math.floor(i/6);
+		var col = i%6;
+		var temp = game.add.button(col*80+600,li*80+200,'button-level', goToLevelPlay, this, 2,1,0,1);
 		temp.number =i+1;
+		temp.addChild(game.make.text(30,30,temp.number,levelStyle));
 	}
 	
-	game.add.button(0,li*80 + 100,'importButton', goToImportJSON, this, 2,1,0,1);
+	//game.add.button(600,li*80 + 100+200,'button-import', goToImportJSON, this, 2,1,0,1);
+}
+
+function winButtons(){
+	var levelStyle = {font: "30px Arial", fontWeight: "bold", fill: "#0D004C"};
+	
+	var curLevel = game.global.levelnum;
+	var again = game.add.button(600,500, 'button-again', goToLevelPlay, this,  2,1, 0, 1 );
+	again.number = curLevel;
+	again.addChild(game.make.text(30,30,again.number,levelStyle));
+	
+	if ( curLevel < levels.length -1){
+		var next = game.add.button(700,500, 'button-next', goToLevelPlay, this,  2,1, 0, 1 );
+		next.number = curLevel+1;
+		next.addChild(game.make.text(30,30,next.number,levelStyle));
+	}
 }
 
 function goToFreePlay(){
@@ -32,8 +54,17 @@ function goToLevelPlay(item){
 	game.state.start('levelPlay');
 }
 
-function goToImportJSON() {
+function goToImportJSON(item) {
 	clicker();
+	// var input = item;
+
+    // var reader = new FileReader();
+    // reader.onload = function(){
+      // var dataURL = reader.result;
+      // var output = document.getElementById('output');
+      // output.src = dataURL;
+    // };
+    //reader.readAdArrayBuffer(input.files[0]);
 	// selection d'un json + go
 }
 
