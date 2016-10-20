672928758f552b93ed155328460d8ecab28e3b61
Oriane <oriane.cosson@gmail.com>
2016-10-20 17:26:34 +0200

    new level buttons with stars

--- script/functions/navigationButtons.js
+++ script/functions/navigationButtons.js
@@ -35,11 +35,13 @@ function levelsButtons(){
 	var levelsNumber = levels.length;
 	
 	for (i=0; i<levelsNumber-1;i++){
-		var li = Math.floor(i/6);
-		var col = i%6;
-		var temp = game.add.button(col*80+600,li*80+200,'button-level', goToLevelPlay, this, 2,1,0,1);
+		var li = Math.floor(i/7);
+		var col = i%7;
+		var temp = game.add.button(col*90+600,li*90+200,'button-level', goToLevelPlay, this, 2,1,0,1);
 		temp.number =i+1;
-		temp.addChild(game.make.text(30,30,temp.number,levelStyle));
+		temp.addChild(game.make.text(30,35,temp.number,levelStyle));
+		var status = (i<game.global.saveData.length) ? game.global.saveData[i] : 0 ;
+		temp.addChild(game.make.sprite(4,5,'level-status',status));
 	}
 	
 	//game.add.button(600,li*80 + 100+200,'button-import', goToImportJSON, this, 2,1,0,1);
@@ -51,14 +53,16 @@ function winButtons(){
 	var levelStyle = {font: "30px Arial", fontWeight: "bold", fill: "#0D004C"};
 	
 	var curLevel = game.global.levelnum;
-	var again = game.add.button(600,500, 'button-again', goToLevelPlay, this,  2,1, 0, 1 );
+	var again = game.add.button(600,500, 'button-level', goToLevelPlay, this,  2,1, 0, 1 );
 	again.number = curLevel;
-	again.addChild(game.make.text(30,30,again.number,levelStyle));
+	again.addChild(game.make.text(30,35,again.number,levelStyle));
+	again.addChild(game.make.sprite(3,5,'level-status',4));
 	
 	if ( curLevel < levels.length -1){
-		var next = game.add.button(700,500, 'button-next', goToLevelPlay, this,  2,1, 0, 1 );
+		var next = game.add.button(700,500, 'button-level', goToLevelPlay, this,  2,1, 0, 1 );
 		next.number = curLevel+1;
-		next.addChild(game.make.text(30,30,next.number,levelStyle));
+		next.addChild(game.make.text(30,35,next.number,levelStyle));
+		next.addChild(game.make.sprite(4,5,'level-status',5));
 	}
 	
 	game.add.sprite(300,300,'win-patrick');
