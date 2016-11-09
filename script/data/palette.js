18a829bda6c20161aa39b7c7141bb4d4df6bb256
Oriane <oriane.cosson@gmail.com>
2016-11-09 15:16:09 +0100

    change colors : deault color and win pannel background

--- script/data/palette.js
+++ script/data/palette.js
@@ -4,7 +4,7 @@ define ([
 
 		return {
             palette : ["0x83EE00", "0xFBFE00", "0x058AB6", "0xE4005C", "0x8007BE", "0xFF8300"],
-			defaultColor : "0x93AFBD",
+			defaultColor : "0xafc7ff",
 			patternColor : "0x000000"
 		};
 
